8da1d54e21cc448a7beb9dfb378e5b78b7222adf
yashoda-fr <fyashodha@gmail.com>
2024-01-08 12:19:37 +0530

    lesson 12

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-
-import Home from './Home';
 import Navbar from './Navbar';
+import Home from './Home';
 
-function App () {
+function App() {
   return (
     <div className="App">
       <Navbar />
       <div className="content">
-       <Home />
-        </div>
+        <Home />
+      </div>
     </div>
   );
 }
+
 export default App;
